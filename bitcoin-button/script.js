2910df1594a20bb0b4226345a7f720315797edcc
Jon Lund Steffensen <jonlst@gmail.com>
2013-03-03 11:53:26 +0100

    script.js: Check the JSON response explicitly.
    Check the data fields of the JSON response before using the value. Also hide the counter until a value has been properly fetched.

--- bitcoin-button/script.js
+++ bitcoin-button/script.js
@@ -6,6 +6,7 @@
 	    var div = $(this).wrap('<div class="bitcoin-div" style="inline-block"/>').parent();
 	    var show_info = $(this).data('info');
 
+	    div.append('<a href="'+url+'" class="bitcoin-counter" style="display:none;">&nbsp;</a>');
 	    div.append('<div class="bitcoin-bubble" style="display:none;"><div><strong>Bitcoin address</strong></div><a href="'+url+'"><img src="http://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=H|0&chl='+encodeURIComponent(url)+'" width="200" height="200" alt="QR code"/></a><div><a href="'+url+'">'+address+'</a></div></div>');
 
 	    div.hover(function () {
@@ -15,15 +16,17 @@
 	    });
 
 	    if (show_info && show_info != 'none') {
-		div.append('<a href="'+url+'" class="bitcoin-counter">&nbsp;</a>');
 		$.get('/wp-content/plugins/bitcoin-button/address-info.php', { address: address }, function (data) {
 		    var counter = div.find('.bitcoin-counter');
-		    if (show_info == 'transaction') {
+		    if (show_info == 'transaction' && data.transactions !== undefined) {
 			counter.text(data.transactions+" tx");
-		    } else if (show_info == 'balance') {
+			counter.show();
+		    } else if (show_info == 'balance' && data.balance !== undefined) {
 			counter.text((data.balance/100000000).toFixed(3)+" ฿");
-		    } else if (show_info == 'received') {
+			counter.show();
+		    } else if (show_info == 'received' && data.received !== undefined) {
 			counter.text((data.received/100000000).toFixed(3)+" ฿")
+			counter.show();
 		    }
 		}, 'json');
 	    }
