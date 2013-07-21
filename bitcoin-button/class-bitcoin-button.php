3ffefadc190222bc6273f8eb7fdf659d57589f96
Tim Neill <neill.tim+github@gmail.com>
2013-07-21 12:28:54 +1000

    Added a bunch of functions and comments, more WP formatting. Fixed some bugs that crept in after the refactor.

--- bitcoin-button/class-bitcoin-button.php
+++ bitcoin-button/class-bitcoin-button.php
@@ -2,9 +2,17 @@
 
 class Bitcoin_Button {
 	
-	protected $address_list = array(); // Contains all addresses used so far
-	protected $blockchain_cache_time = 1800;
+	private   $endpoint_url = 'http://blockchain.info/address/';
 	
+	protected $address_list = array(); // Contains whitelisted addresses (used by shortcodes). Prevents unauthorised requests
+	protected $cache_time   = 1800;    // 30 minutes cache time
+	protected $format       = 'json';  // Currently only one format
+	protected $limit        = 0;       // Currently no limit
+	
+	
+	/**
+	 * Bootstrap plugin actions
+	 */
 	public function __construct() {
 		// Front-end only actions
 		if ( !is_admin() ) {
@@ -20,12 +28,24 @@ class Bitcoin_Button {
 		$this->address_list = get_option( 'bitcoin_address_list', array() );
 	}
 	
+	
+	/**
+	 * Add resource scripts to the frontend and set up ajax endpoint
+	 *
+	 * @return void
+	 */
 	public function load_scripts() {
 		wp_enqueue_style( 'bitcoin-button', plugins_url( 'style.css', __FILE__ ) );
 		wp_enqueue_script( 'bitcoin-button', plugins_url( 'script.js', __FILE__ ), array( 'jquery' ) );
 		wp_localize_script( 'bitcoin-button', 'bitcoin_button', array( 'url' => admin_url( 'admin-ajax.php' ) ) );
 	}
 	
+	
+	/**
+	 * Make an external request to the endpoint (blockchain) and parse the results
+	 *
+	 * @return void
+	 */
 	public function get_address_info() {
 		$address = @$_GET['address'];
 		
@@ -35,39 +55,51 @@ class Bitcoin_Button {
 		}
 
 		$output = array( 'address' => $address );
-		$data = get_transient('btcaddr-' . $address); // Previous blockchain request
+		$data = get_transient("btcaddr-{$address}"); // Previous blockchain request
 
 		if ( $data === false ) {
-			$response = wp_remote_get( 'http://blockchain.info/address/' . urlencode( $address ) . '?format=json&limit=0' );
+			$response = wp_remote_get( $this->get_request_url( $address ) );
 			$code = wp_remote_retrieve_response_code( $response );
 
-			if ( $code !== 200 )
+			if ( $code !== 200 ) {
 				$this->send_data( $output );
+			}
 
 			$data = json_decode( wp_remote_retrieve_body( $response ) );
 
-			set_transient('btcaddr-' . $address, $data, $this->blockchain_cache_time);
+			set_transient("btcaddr-{$address}", $data, $this->cache_time);
 		}
 
 		// If there's a bad address, abort
-		if ( ! isset( $data->address ) || $data->address != $address )
+		if ( ! isset( $data->address ) || $data->address != $address ) {
 			$this->send_data();
+		}
 
 		// Transaction data
-		if ( isset( $data->n_tx ) )
+		if ( isset( $data->n_tx ) ) {
 			$output['transactions'] = intval( $data->n_tx );
+		}
 
 		// Balance data
-		if ( isset( $data->final_balance ) )
+		if ( isset( $data->final_balance ) ) {
 			$output['balance'] = intval( $data->final_balance );
+		}
 
 		// Total data
-		if ( isset( $data->total_received ) )
+		if ( isset( $data->total_received ) ) {
 			$output['received'] = intval( $data->total_received );
+		}
 
-		$this->send_data( $data );
+		$this->send_data( $output );
 	}
 	
+	
+	/**
+	 * Handle shortcode parsing
+	 *
+	 * @param array $atts
+	 * @return string
+	 */
 	public function do_shortcode( $atts ) {
 		// Output
 		$html = '';
@@ -90,6 +122,7 @@ class Bitcoin_Button {
 
 		// Build bitcoin url with optional params
 		$url = "bitcoin:{$address}";
+		
 		if ( $query = http_build_query( $params ) ) {
 			$url .= "?{$query}";
 		}
@@ -100,7 +133,7 @@ class Bitcoin_Button {
 		$this->update_address_list();
 		
 		if ( ! is_feed() ) {
-			$html = '<a class="bitcoin-button" data-address="' . $address . '" data-info="' . $info . '" href="' . $url . '">Bitcoin</a>';
+			$html = '<a class="bitcoin-button" data-address="' . $address . '" data-info="' . $info . '" href="' . $url . '"><span class="label">Bitcoin</span></a>';
 		} else {
 			$html = 'Bitcoin: <a href="' . $url . '">' . (isset( $params['label'] ) && $params['label'] ? $params['label'] : $address) . '</a>';
 		}
@@ -108,17 +141,48 @@ class Bitcoin_Button {
 		return $html;
 	}
 	
+	
+	/**
+	 * Generate remote request URL
+	 *
+	 * @param string $address The bitcoin address to look up
+	 * @return string
+	 */
+	protected function get_request_url( $address ) {
+		return $this->endpoint_url . urlencode( $address ) . '?format=' . $this->format . '&limit=' . intval($this->limit);
+	}
+	
+	
+	/**
+	 * If the address list has changed, update it
+	 *
+	 * @return void
+	 */
 	protected function update_address_list() {
 		if ( count( $this->address_list ) ) {
 			update_option( 'bitcoin_address_list', $this->address_list );
 		}
 	}
 	
+	
+	/**
+	 * Add a unique address to the whitelist
+	 *
+	 * @param string $address 
+	 * @return void
+	 */
 	protected function add_address( $address ) {
 		$this->address_list[] = $address;
 		array_unique( $this->address_list );
 	}
 	
+	
+	/**
+	 * Cancels an XHR request in the event the address isn't in the whitelist
+	 *
+	 * @param string $address 
+	 * @return void
+	 */
 	protected function is_valid_address( $address ) {
 		// Only allow external queries that have been explicitly allowed.
 		if ( ! in_array( $address, $this->address_list ) ) {
@@ -126,12 +190,19 @@ class Bitcoin_Button {
 		}
 	}
 	
+	
+	/**
+	 * Send JSON data to the client. Cache headers also sent
+	 *
+	 * @param mixed $data 
+	 * @return void
+	 */
 	protected function send_data( $data ) {
 		/**
 		 * 200: OK
 		 */
 		header( 'Content-Type: application/json' );
-		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + $this->blockchain_cache_time ) . ' GMT' );
+		header( 'Expires: ' . gmdate( 'D, d M Y H:i:s', time() + $this->cache_time ) . ' GMT' );
 		status_header( 200 );
 		
 		echo json_encode( $data );
@@ -139,6 +210,13 @@ class Bitcoin_Button {
 		exit;
 	}
 	
+	
+	/**
+	 * Send a failure and an arbitrary HTTP status code to the client
+	 *
+	 * @param int|string $code 
+	 * @return void
+	 */
 	protected function send_failure( $code = 404 ) {
 		/**
 		 * 400: Bad request
--- bitcoin-button/script.js
+++ bitcoin-button/script.js
@@ -1,36 +1,29 @@
+// Wrap jQuery, undefined
 (function($, und) {
 	$(function() {
-		$('.bitcoin-button').each(function () {
+		$('.bitcoin-button').each(function() {
 			var $this     = $(this),
-			    url       = $this.href,
+			    $wrapper  = $this.wrap('<div class="bitcoin-wrap" />').parent()
+			    url       = this.href,
 			    address   = $this.data('address'),
 			    show_info = $this.data('info'),
-			    wrapper   = $this.wrap('<div class="bitcoin-wrap" />').parent();
-		
-			wrapper.append('<a href="' + url + '" class="bitcoin-counter" style="display:none;">&nbsp;</a>');
-			wrapper.append('<div class="bitcoin-bubble" style="display:none;"><a href="' + url + '"><img src="http://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=H|0&chl=' + encodeURIComponent(url) + '" class="qr-code" width="200" height="200" alt="" /><span class="address">' + address + '</span></div>');
-		
-			wrapper.hover(
-				function () {
-					$this.find('.bitcoin-bubble').fadeIn(150);
-				},
-				function () {
-					$this.find('.bitcoin-bubble').fadeOut(150);
-				}
-			);
-		
+			    $counter  = $('<a href="' + url + '" class="bitcoin-counter"></a>'),
+			    $bubble   = $('<div class="bitcoin-bubble" style="display:none;"><a href="' + url + '"><img src="http://chart.googleapis.com/chart?chs=200x200&cht=qr&chld=H|0&chl=' + encodeURIComponent(url) + '" class="qr-code" width="200" height="200" alt="" /><span class="address">' + address + '</span></div>');
+
+			$wrapper.append($bubble);
+			$wrapper.hover(function() { $bubble.show(); }, function() { $bubble.hide(); });
+
 			if ( show_info && show_info != 'none' ) {
-				$.get(bitcoin_button.url, { action: 'bitcoin-address-info', address: address }, function (data) {
-				    var counter = wrapper.find('.bitcoin-counter');
-				    if (show_info == 'transaction' && data.transactions !== und) {
-						counter.text(data.transactions+" tx");
-						counter.show();
-				    } else if (show_info == 'balance' && data.balance !== und) {
-						counter.text((data.balance/100000000).toFixed(3)+" ฿");
-						counter.show();
-				    } else if (show_info == 'received' && data.received !== und) {
-						counter.text((data.received/100000000).toFixed(3)+" ฿")
-						counter.show();
+				$.get(bitcoin_button.url, { action: 'bitcoin-address-info', address: address }, function(data) {
+				    if ( show_info == 'transaction' && data.transactions !== und ) {
+						$counter.text(data.transactions + " tx");
+						$counter.insertBefore($bubble);
+				    } else if ( show_info == 'balance' && data.balance !== und ) {
+						$counter.text((data.balance / 100000000).toFixed(3) + " ฿");
+						$counter.insertBefore($bubble);
+				    } else if ( show_info == 'received' && data.received !== und ) {
+						$counter.text((data.received / 100000000).toFixed(3) + " ฿")
+						$counter.insertBefore($bubble);
 				    }
 				}, 'json');
 			}
