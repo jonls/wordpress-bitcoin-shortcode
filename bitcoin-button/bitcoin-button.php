fe6bd366e23698b9d415d55ae291a1bc6e080b07
Jon Lund Steffensen <jonlst@gmail.com>
2014-05-04 14:47:26 -0400

    Bump version to 0.8

--- bitcoin-button/bitcoin-button.php
+++ bitcoin-button/bitcoin-button.php
@@ -2,14 +2,14 @@
 /**
  * @package jonls.dk-Bitcoin-Button
  * @author Jon Lund Steffensen
- * @version 0.7
+ * @version 0.8
  */
 /*
 Plugin Name: jonls.dk-Bitcoin-Button
 Plugin URI: http://jonls.dk/
 Description: Shortcode for inserting a bitcoin button
 Author: Jon Lund Steffensen
-Version: 0.7
+Version: 0.8
 Author URI: http://jonls.dk/
 */
 
